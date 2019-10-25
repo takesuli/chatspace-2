9bd6e366cab449dff5c710ff7dc5c380c6c1099b
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-10-25 10:05:24 +0900

    push

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,80 +1,79 @@
 $(function(){
-  function buildGroup(message){
-    var text = message.content ? `${message.content}` : " " ; 
-    var image = message.image ? `<img class="main_contents_box" src="${message.image}">`: " ";
-    var html = `<div class="main_contents_box" data-message-id="${message.id}">
-                  <div class="main_contents__chatspace__head">
-                    <div class="main_contents__chatspace__head__user_name">
-                      ${message.user_name}
+  $(document).on('turbolinks:load', function() {
+    function buildGroup(message){
+      var text = message.content ? `${message.content}` : " " ; 
+      var image = message.image ? `<img class="main_contents_box" src="${message.image}">`: " ";
+      var html = `<div class="main_contents_box" data-message-id="${message.id}">
+                    <div class="main_contents__chatspace__head">
+                      <div class="main_contents__chatspace__head__user_name">
+                        ${message.user_name}
+                      </div>
+                      <div class="main_contents__chatspace__head__time">
+                        ${message.created_at}
+                      </div>
                     </div>
-                    <div class="main_contents__chatspace__head__time">
-                      ${message.created_at}
+                    <div class="main_contents__chatspace__text">
+                      <p class="main_contents__chatspace__text">
+                        ${text}
+                      </p>
+                        ${image}
                     </div>
-                  </div>
-                  <div class="main_contents__chatspace__text">
-                    <p class="main_contents__chatspace__text">
-                      ${text}
-                    </p>
-                      ${image}
-                  </div>
-                </div>`
-  
-    return html;
-  };
-  $('#new_message').on('submit', function(e){
-    e.preventDefault();
+                  </div>`
+    
+      return html;
+    };
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
 
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
 
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(message){
-      console.log(message);
-      var html = buildGroup(message);
-      $(".main_contents__chatspace").append(html)
-      $('.main_contents__form__submit').attr('disabled', false);
-      $('.main_contents__chatspace').animate({ scrollTop: $('.main_contents__chatspace')[0].scrollHeight});
-      $('#new_message')[0].reset();
-    })
-    .fail(function(){
-      alert('error');
-    })
-  })
-
-  var reloadMessages = function() {
-    if (window.location.href.match(/\/groups\/\d+\/messages/)){  
-      message_id = $(".main_contents_box:last").data('message-id');
       $.ajax({
-        url: 'api/messages#index {:format=>"json"}',
-        type: 'get',
+        url: url,
+        type: "POST",
+        data: formData,
         dataType: 'json',
-        data: {id: message_id}
+        processData: false,
+        contentType: false
       })
+      .done(function(message){
+        var html = buildGroup(message);
+        $(".main_contents__chatspace").append(html)
+        $('.main_contents__form__submit').attr('disabled', false);
+        $('.main_contents__chatspace').animate({ scrollTop: $('.main_contents__chatspace')[0].scrollHeight});
+        $('#new_message')[0].reset();
+      })
+      .fail(function(){
+        alert('error');
+      })
+    })
 
-      .done(function(messages) {
-          //追加するHTMLの入れ物を作る
-        var insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        messages.forEach(function(message){
-          insertHTML = buildGroup(message);
-          $('.main_contents__chatspace').append(insertHTML);
-          $('.main_contents__chatspace').animate({scrollTop: $('.main_contents__chatspace')[0].scrollHeight}, 'fast');
-          $('#new_message')[0].reset();
+    var reloadMessages = function() {
+      if (window.location.href.match(/\/groups\/\d+\/messages/)){  
+        message_id = $(".main_contents_box:last").data('message-id');
+        $.ajax({
+          url: 'api/messages#index {:format=>"json"}',
+          type: 'get',
+          dataType: 'json',
+          data: {id: message_id}
+        })
+
+        .done(function(messages) {
+          var insertHTML = '';
+          messages.forEach(function(message){
+            insertHTML = buildGroup(message);
+            $('.main_contents__chatspace').append(insertHTML);
+            $('.main_contents__chatspace').animate({scrollTop: $('.main_contents__chatspace')[0].scrollHeight}, 'fast');
+            $('#new_message')[0].reset();
+          });
+        })
+        .fail(function() {
+          alert('error');      
         });
-      })
-      .fail(function() {
-        alert('error');      
-      });
+      }
     }
-  }
-  setInterval(reloadMessages, 5000);
+    setInterval(reloadMessages, 5000);
+  });
 });
 
     
--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,67 +1,69 @@
 $(function() {
+  $(document).on('turbolinks:load', function() {
+        // ...
+      
 
-var name_list = $("#user-search-result");
-var member_list =$("#user-member-list");
 
-function appendName(user) {
-var html = `<div class="chat-group-user clearfix">
-                   <p class="chat-group-user__name">${user.name}</p>
-                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
-                 </div>`
-    name_list.append(html);
-}
+    var name_list = $("#user-search-result");
+    var member_list =$("#user-member-list");
 
-function appendMember(name,id) {
-  var html = `<div class="chat-group-user clearfix">
-                     <input name="group[user_ids][]" type="hidden" value="${id}">
-                     <p class="chat-group-user__name">${name}</p>
-                     <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}" data-user-name="${name}">削除</a>
-                   </div>`
-                   
-      member_list.append(html);
-  }
+    function appendName(user) {
+    var html = `<div class="chat-group-user clearfix">
+                      <p class="chat-group-user__name">${user.name}</p>
+                      <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                    </div>`
+        name_list.append(html);
+    }
 
-  $("#user-search-field").on ("keyup",  function() {
-    var input = $("#user-search-field").val();
-    console.log(input)
-    $.ajax({
-      type: 'GET',
-      url: '/users',
-      data: { keyword: input },
-      dataType: 'json'
-    })
+    function appendMember(name,id) {
+      var html = `<div class="chat-group-user clearfix">
+                        <input name="group[user_ids][]" type="hidden" value="${id}">
+                        <p class="chat-group-user__name">${name}</p>
+                        <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove" data-user-id="${id}" data-user-name="${name}">削除</a>
+                      </div>`
+                      
+          member_list.append(html);
+      }
 
-    .done(function(users) {
-      $("#user-search-result").empty();
-      if (users.length !== 0) {
-        users.forEach(function(user){
-          appendName(user);
+      $("#user-search-field").on ("keyup",  function() {
+        var input = $("#user-search-field").val();
+        
+        $.ajax({
+          type: 'GET',
+          url: '/users',
+          data: { keyword: input },
+          dataType: 'json'
+        })
+
+        .done(function(users) {
+          $("#user-search-result").empty();
+          if (users.length !== 0) {
+            users.forEach(function(user){
+              appendName(user);
+            });
+          }
+          else {
+            $("#user-search-result").append("一致するグループはありません");
+          }
+        })
+        .fail(function() {
+          alert("error");
         });
-      }
-      else {
-        $("#user-search-result").append("一致するグループはありません");
-      }
-    })
-    .fail(function() {
-      alert("error");
-    });
+      });
+
+      $('#user-search-result').on ('click', '.chat-group-user__btn--add' ,function(e){
+        var name = $(this).data('user-name');
+        var id =$(this).data('user-id');
+        // if appendMember
+        appendMember(name,id);
+      })
+
+      $('#user-search-result').on ('click', '.chat-group-user__btn--add' ,function(e){
+        $(this).parent().remove(); 
+      })
+
+
+      $('#chat-group-user')
   });
 
-  $('#user-search-result').on ('click', '.chat-group-user__btn--add' ,function(e){
-    var name = $(this).data('user-name');
-    var id =$(this).data('user-id');
-    // if appendMember
-    appendMember(name,id);
-  })
-
-  $('#user-search-result').on ('click', '.chat-group-user__btn--add' ,function(e){
-    $(this).parent().remove(); 
-  })
-
-  $('#user-member-list').on ('click', '.chat-group-user__btn--remove' ,function(e){
-    $(this).parent().remove(); 
-  })
-
-  $('#chat-group-user')
-});
-
+})
