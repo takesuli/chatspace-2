1bfe28f8c6a5a0f9c7d7f2c6d670b4d69726c6b5
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-10-31 14:10:35 +0900

    z

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,6 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1' #例 'ap-northeast-1'
   }
 
-  config.fog_directory  = 'chat-application-chatspace-images'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-application-chatspace-images'
+  config.fog_directory  = 'chatspacesimages'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chatspacesimages'
 end
