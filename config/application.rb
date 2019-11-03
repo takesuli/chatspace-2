7da5eda0566e93168a2956514845e5ba81192c9c
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-11-03 16:32:18 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Chatspace
       g.test_framework false
     end
   config.time_zone = 'Tokyo'
-  config.active_record.default_timezone = :local
+  # config.active_record.default_timezone = :local
   end
 end
 
