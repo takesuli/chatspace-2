e47a3689289cc9e02240b254b93a8d52159dadd0
yamazakitakehiro <ndabadaaaaaa@yahoo.co.jp>
2019-11-03 16:21:37 +0900

    time update

--- config/application.rb
+++ config/application.rb
@@ -14,18 +14,8 @@ module Chatspace
       g.helper false
       g.test_framework false
     end
-    config.i18n.default_locale = :ja
-    config.time_zone = 'Asia/Tokyo'
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
   end
 end
 
-module PairsLike
-  class Application < Rails::Application
-    # ここから下を追加
-    config.generators do |g|
-      g.javascripts false
-      g.helper false
-      g.test_framework false
-    end
-  end
-end
